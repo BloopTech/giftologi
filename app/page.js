5c544e56766f4d8af207f7c65857a78f769c6e92
GodsonAddy <gaddy@virtualmindstechnology.com>
2025-09-03 14:01:27 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,13 +7,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-[#16150FB2] text-black flex flex-col min-h-screen gap-16 pt-5 px-10 w-full items-center justify-center"
-    style={{
-      backgroundImage: "url('/coming_soon_layer.png')",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    }}
+    <div
+      className="bg-[#16150FB2] text-black flex flex-col min-h-screen gap-16 pt-5 px-10 w-full items-center justify-center"
+      style={{
+        backgroundImage: "url('/coming_soon_layer.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
     >
       <main className="flex flex-col items-center justify-center w-full">
         <div className="rounded-2xl relative overflow-hidden max-w-md mx-auto bg-[#FFFCEF] fade-in-up delay-3 border border-transparent font-poppins">
@@ -21,9 +22,14 @@ export default function Home() {
             aria-hidden="true"
             className="pointer-events-none absolute top-[55px] -right-2 w-[100%] h-[10px] bg-[#D2BF7C] origin-top-right rotate-40 z-1"
           />
-          <div className="relative z-10 p-8 flex items-center space-x-8 justify-center w-full">
+          <div className="relative z-10 p-8 flex flex-col md:flex-row space-x-0 space-y-4 md:space-y-0 items-center md:space-x-8 justify-center w-full">
             <div className="relative z-10">
-              <Image src={Logo} alt="Logo" width={120} height={120} />
+              <Image
+                src={Logo}
+                alt="Logo"
+                className="w-20 h-20 md:w-30 md:h-30"
+                priority
+              />
             </div>
             <div className="relative z-10 flex flex-col space-y-4 w-full">
               <p className="text-[#85753C] font-semibold">
@@ -35,18 +41,18 @@ export default function Home() {
                 We&apos;re handpicking every detail, just for you. Sit
                 tight—we&apos;ll be live soon.
               </p>
-              <div className="flex space-x-4 w-full">
+              <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 w-full">
                 <Link
-                  href="mailto:devs@mygiftologi.com"
+                  href="mailto:hello@mygiftologi.com"
                   className="rounded-xl flex items-center justify-center px-4 py-2 bg-[#A5914B] text-xs text-white"
                   aria-label="Send us an email"
                 >
                   Send us an Email
                 </Link>
                 <Link
-                  href="tel:+233244151619"
+                  href="tel:+233598608892"
                   className="rounded-xl flex items-center justify-center px-8 py-2 bg-[#85753C] text-xs text-white"
-                  aria-label="Call us at +233244151619"
+                  aria-label="Call us at +233598608892"
                 >
                   Call Us
                 </Link>
